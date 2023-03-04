0cc3f24d0dc58dda3b3cbf02fc449835fcdf9497
Nelson Estevão <nelson@marmelasoft.com>
2023-03-04 23:50:54 +0000

    Add simple utils related to ecto

--- lib/utils.ex
+++ lib/utils.ex
@@ -1,18 +1,2 @@
 defmodule Utils do
-  @moduledoc """
-  Documentation for `Utils`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Utils.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Utils.MixProject do
   def project do
     [
       app: :utils,
-      version: "0.1.0",
+      version: "0.0.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -21,8 +21,7 @@ defmodule Utils.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ecto_sql, "~> 3.9"}
     ]
   end
 end
