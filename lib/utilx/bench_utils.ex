e61925de4ac0a10d737d8470ab2519408ff125da
Nelson Estevão <nelson@marmelasoft.com>
2023-06-10 00:19:29 +0100

    Improve BenchUtils

--- lib/utilx/bench_utils.ex
+++ lib/utilx/bench_utils.ex
@@ -1,6 +1,17 @@
 defmodule Utilx.BenchUtils do
-  require Timex
+  @moduledoc """
+  Utility for quick and simple benchmarking functions in Elixir.
+  """
 
+  @doc """
+  Benchmarks the execution time of a piece of code. It should be used only in
+  really simple cases.
+
+  ## Examples
+
+    iex> timeit "Sleep for a while", do: :timer.sleep(1000)
+    Executed Sleep for a while in 1.00 seconds
+  """
   defmacro timeit(name, do: yield) do
     quote do
       {time, value} =
@@ -8,13 +19,15 @@ defmodule Utilx.BenchUtils do
           unquote(yield)
         end)
 
-      time_humanized =
-        Timex.Duration.from_seconds(time / 1_000_000)
-        |> Timex.Format.Duration.Formatter.format(:humanized)
-
-      IO.puts("Executed #{unquote(name)} in #{time_humanized}")
+      IO.puts("Executed #{unquote(name)} in #{humanize(time)}")
 
       value
     end
   end
+
+  defp humanize(time_us) when time_us < 1_000, do: "#{time_us} μs"
+
+  defp humanize(time_us) when time_us < 1_000_000, do: "#{Float.round(time_us / 1_000, 2)} ms"
+
+  defp humanize(time_us), do: "#{Float.round(time_us / 1_000_000, 2)} s"
 end
