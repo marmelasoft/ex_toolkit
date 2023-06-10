5df9dd256d12fe8be65310e843d5bafb3b9119f8
Nelson Estevão <nelson@marmelasoft.com>
2023-06-10 12:40:58 +0100

    Release 0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Utilx.MixProject do
 
   @app :utilx
   @name "Utilx"
-  @version "0.1.5"
+  @version "0.2.0"
   @description "Collection of effective recipes for building Elixir applications"
   @scm_url "https://github.com/marmelasoft/ex_utils"
 
