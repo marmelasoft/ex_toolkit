ad517620b12217f37e749d7111d0aaa849f4bb13
Nelson Estevão <nelson@marmelasoft.com>
2024-01-13 16:09:43 +0000

    feat: add SlugID

---
+++ lib/ex_toolkit/ecto/slug_id.ex
@@ -0,0 +1,70 @@
+defmodule ExToolkit.Ecto.SlugID do
+  @moduledoc """
+  A Ecto type that facilitates the generation of prefixed base62 encoded UUIDv7
+  for use as primary and foreign keys in Ecto schemas.
+
+  ## Examples
+
+      @primary_key {:id, ExToolkit.Ecto.SlugID, autogenerate: true}
+      @foreign_key_type ExToolkit.Ecto.SlugID
+  """
+  use Ecto.ParameterizedType
+
+  alias ExToolkit.Encode.Base62UUID
+
+  @impl true
+  def init(opts), do: opts
+
+  @impl true
+  def type(_params), do: :uuid
+
+  @impl true
+  def cast(nil, _params), do: {:ok, nil}
+
+  def cast(data, _params) do
+    with {:ok, _uuid} <- slug_to_uuid(data) do
+      {:ok, data}
+    else
+      _ -> :error
+    end
+  end
+
+  defp slug_to_uuid(slug) do
+    with {:ok, uuid} <- Base62UUID.decode(slug) do
+      {:ok, uuid}
+    else
+      _ -> :error
+    end
+  end
+
+  defp uuid_to_slug(uuid), do: Base62UUID.encode(uuid)
+
+  @impl true
+  def load(data, _loader, _params) do
+    case UUIDv7.load(data) do
+      {:ok, uuid} -> {:ok, uuid_to_slug(uuid)}
+      :error -> :error
+    end
+  end
+
+  @impl true
+  def dump(nil, _, _), do: {:ok, nil}
+
+  def dump(slug, _dumper, _params) do
+    case slug_to_uuid(slug) do
+      {:ok, uuid} -> UUIDv7.dump(uuid)
+      :error -> :error
+    end
+  end
+
+  @impl true
+  def autogenerate(_params) do
+    uuid_to_slug(UUIDv7.autogenerate())
+  end
+
+  @impl true
+  def embed_as(format, _params), do: UUIDv7.embed_as(format)
+
+  @impl true
+  def equal?(a, b, _params), do: UUIDv7.equal?(a, b)
+end
---
+++ test/ex_toolkit/ecto/slug_id_test.exs
@@ -0,0 +1,92 @@
+defmodule ExToolkit.Ecto.SlugIDTest do
+  @moduledoc false
+  use ExUnit.Case, async: true
+
+  alias ExToolkit.Ecto.SlugID
+  alias Uniq.UUID
+
+  defmodule TestSchema do
+    @moduledoc false
+    use Ecto.Schema
+
+    @primary_key {:id, SlugID, autogenerate: true}
+    @foreign_key_type SlugID
+
+    schema "test" do
+      belongs_to(:test, TestSchema)
+    end
+  end
+
+  @params SlugID.init(
+            schema: TestSchema,
+            field: :id,
+            primary_key: true,
+            autogenerate: true
+          )
+  @belongs_to_params SlugID.init(schema: TestSchema, field: :test, foreign_key: :test_id)
+  @loader nil
+  @dumper nil
+
+  @test_prefixed_uuid "3TUIKuXX5mNO2jSA41bsDx"
+  @test_uuid UUID.to_string("7232b37d-fc13-44c0-8e1b-9a5a07e24921", :raw)
+  @test_prefixed_uuid_with_leading_zero "02tREKF6r6OCO2sdSjpyTm"
+  @test_uuid_with_leading_zero UUID.to_string("0188a516-bc8c-7c5a-9b68-12651f558b9e", :raw)
+  @test_prefixed_uuid_null "0000000000000000000000"
+  @test_uuid_null UUID.to_string("00000000-0000-0000-0000-000000000000", :raw)
+  @test_prefixed_uuid_invalid_characters String.duplicate(".", 32)
+  @test_uuid_invalid_characters String.duplicate(".", 22)
+  @test_prefixed_uuid_invalid_format String.duplicate("x", 31)
+  @test_uuid_invalid_format String.duplicate("x", 21)
+
+  test "cast/2" do
+    assert SlugID.cast(@test_prefixed_uuid, @params) == {:ok, @test_prefixed_uuid}
+
+    assert SlugID.cast(@test_prefixed_uuid_with_leading_zero, @params) ==
+             {:ok, @test_prefixed_uuid_with_leading_zero}
+
+    assert SlugID.cast(@test_prefixed_uuid_null, @params) == {:ok, @test_prefixed_uuid_null}
+    assert SlugID.cast(nil, @params) == {:ok, nil}
+    assert SlugID.cast(@test_prefixed_uuid_invalid_characters, @params) == :error
+    assert SlugID.cast(@test_prefixed_uuid_invalid_format, @params) == :error
+    assert SlugID.cast(@test_prefixed_uuid, @belongs_to_params) == {:ok, @test_prefixed_uuid}
+  end
+
+  test "load/3" do
+    assert SlugID.load(@test_uuid, @loader, @params) == {:ok, @test_prefixed_uuid}
+
+    assert SlugID.load(@test_uuid_with_leading_zero, @loader, @params) ==
+             {:ok, @test_prefixed_uuid_with_leading_zero}
+
+    assert SlugID.load(@test_uuid_null, @loader, @params) == {:ok, @test_prefixed_uuid_null}
+    assert SlugID.load(@test_uuid_invalid_characters, @loader, @params) == :error
+    assert SlugID.load(@test_uuid_invalid_format, @loader, @params) == :error
+    assert SlugID.load(@test_prefixed_uuid, @loader, @params) == :error
+    assert SlugID.load(@test_uuid, @loader, @belongs_to_params) == {:ok, @test_prefixed_uuid}
+  end
+
+  test "dump/3" do
+    assert SlugID.dump(@test_prefixed_uuid, @dumper, @params) == {:ok, @test_uuid}
+
+    assert SlugID.dump(@test_prefixed_uuid_with_leading_zero, @dumper, @params) ==
+             {:ok, @test_uuid_with_leading_zero}
+
+    assert SlugID.dump(@test_prefixed_uuid_null, @dumper, @params) == {:ok, @test_uuid_null}
+    assert SlugID.dump(@test_uuid, @dumper, @params) == :error
+    assert SlugID.dump(nil, @dumper, @params) == {:ok, nil}
+    assert SlugID.dump(@test_prefixed_uuid, @dumper, @belongs_to_params) == {:ok, @test_uuid}
+  end
+
+  test "autogenerate/1" do
+    assert prefixed_uuid = SlugID.autogenerate(@params)
+    assert {:ok, uuid} = SlugID.dump(prefixed_uuid, nil, @params)
+    assert {:ok, %UUID{format: :raw, version: 7}} = UUID.parse(uuid)
+  end
+
+  test "embed_as/2" do
+    assert :self = SlugID.embed_as(:raw, @params)
+  end
+
+  test "equal/2" do
+    assert SlugID.equal?(@test_uuid, @test_uuid, @params)
+  end
+end
