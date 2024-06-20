a2edc835507a93f1a0386983c02fb487dadc3f59
Nelson Estevão <nelson@marmelasoft.com>
2024-06-20 22:36:51 +0100

    Release 0.9.3

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ The package can be installed by adding `ex_toolkit` to your list of dependencies
 ```elixir
 def deps do
   [
-    {:ex_toolkit, "~> 0.9.2"}
+    {:ex_toolkit, "~> 0.9.3"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExToolkit.MixProject do
 
   @app :ex_toolkit
   @name "ExToolkit"
-  @version "0.9.2"
+  @version "0.9.3"
   @description "Collection of effective recipes for building Elixir applications"
   @scm_url "https://github.com/marmelasoft/ex_toolkit"
 
