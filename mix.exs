d1fec0e5173f59394676050d96b004f6164ee77a
Nelson Estevão <nelson@marmelasoft.com>
2024-06-28 11:39:18 +0100

    Release 0.9.6

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExToolkit.MixProject do
 
   @app :ex_toolkit
   @name "ExToolkit"
-  @version "0.9.5"
+  @version "0.9.6"
   @description "Collection of effective recipes for building Elixir applications"
   @scm_url "https://github.com/marmelasoft/ex_toolkit"
 
